a234fc3128ae479162b7c93832b22d20344078c8
Davi-Garcia-Bosso <150704160+Davi-Garcia-Bosso@users.noreply.github.com>
2024-01-29 17:58:05 -0300

    Adicionando producer e consumer e conectando-os ao rabbitMQ

--- src/modules/express.js
+++ src/modules/express.js
@@ -1,5 +1,5 @@
-// express.js
 const express = require("express");
+const amqp = require("amqplib/callback_api");
 const NumberModel = require("../models/number.model");
 
 const app = express();
@@ -15,6 +15,33 @@ app.use((req, res, next) => {
 
   next();
 });
+
+// Função para enviar mensagem para RabbitMQ
+function sendToQueue(message) {
+  amqp.connect("amqp://localhost", function (error0, connection) {
+    if (error0) {
+      throw error0;
+    }
+
+    connection.createChannel(function (error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+
+      const exchange = "Soma"; // Nome da sua exchange
+
+      channel.assertExchange(exchange, "direct", {
+        durable: false,
+      });
+
+      channel.publish(exchange, "", Buffer.from(message));
+
+      setTimeout(function () {
+        connection.close();
+      }, 500);
+    });
+  });
+}
 
 app.post("/calcularSoma", async (req, res) => {
   const num1 = parseFloat(req.body.num1);
@@ -36,6 +63,10 @@ app.post("/calcularSoma", async (req, res) => {
 
   try {
     const savedNumber = await number.save();
+
+    // Envia o resultado para a exchange no RabbitMQ
+    sendToQueue(JSON.stringify({ resultado, number: savedNumber }));
+
     res.json({ resultado, number: savedNumber });
   } catch (error) {
     console.error("Erro ao salvar no banco de dados:", error);
