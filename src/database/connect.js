178c3d425ae245f0667601f356f9dd9d709c03ea
Davi-Garcia-Bosso <150704160+Davi-Garcia-Bosso@users.noreply.github.com>
2024-03-01 19:02:51 -0300

    Erros

--- src/database/connect.js
+++ src/database/connect.js
@@ -3,11 +3,22 @@ const mongoose = require("mongoose");
 const connectToDataBase = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cursonodejs.nqgz2kf.mongodb.net/database?retryWrites=true&w=majority`
+      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cursonodejs.nqgz2kf.mongodb.net/database?retryWrites=true&w=majority`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
     );
-    console.log("Conexão efetuada com sucesso!");
+
+    // Verifica o status da conexão
+    if (mongoose.connection.readyState === 1) {
+      console.log("Conexão efetuada com sucesso!");
+    } else {
+      console.error(
+        "A conexão não foi bem-sucedida. Estado da conexão:",
+        mongoose.connection.readyState
+      );
+    }
   } catch (error) {
     console.error("Ocorreu um erro ao realizar a conexão! Erro: ", error);
+    throw error; // Lança o erro novamente para que chamadores possam tratá-lo
   }
 };
 
